32305a5b6838a1418a334ee5889e82ae1dd7fe03
Juan Trujillo <juan.trujillo@alternova.com>
2024-09-26 17:35:57 -0500

    test: add tests for base notifier and client

---
+++ tests/base_notifier_test.py
@@ -0,0 +1,60 @@
+from unittest import TestCase
+
+from base_notifier import BaseNotifier
+
+
+class TestBaseNotifier(BaseNotifier):
+    """TestBaseNotifier"""
+
+    def send_sms_notification(self, phone_number: str, message: str, **kwargs) -> str:
+        """Sends a SMS notification to the given phone number"""
+        return "SMS notification sent"
+
+    def send_email_notification(
+        self,
+        *,
+        subject: str,
+        email_data: dict,
+        recipients: list,
+        sender: str,
+        template: str,
+        cc_emails: list = None,
+        bcc_emails: list = None,
+        **kwargs,
+    ) -> str:
+        """Sends an email notification to the given email"""
+        return "Email notification sent"
+
+    def send_push_notification(self, message: str, **kwargs) -> str:
+        """Sends a push notification to the given message"""
+        return "Push notification sent"
+
+
+class TestBaseNotifierTests(TestCase):
+    """TestBaseNotifierTests"""
+
+    def test_send_sms_notification(self):
+        """Test send_sms_notification"""
+        notifier = TestBaseNotifier()
+        result = notifier.send_sms_notification(
+            phone_number="123456789", message="Test message"
+        )
+        self.assertEqual(result, "SMS notification sent")
+
+    def test_send_email_notification(self):
+        """Test send_email_notification"""
+        notifier = TestBaseNotifier()
+        result = notifier.send_email_notification(
+            subject="Test subject",
+            email_data={"name": "John Doe"},
+            recipients=["juan.trujillo@alternova.com"],
+            sender="test.service@alternovastudio.com",
+            template="test",
+        )
+        self.assertEqual(result, "Email notification sent")
+
+    def test_send_push_notification(self):
+        """Test send_push_notification"""
+        notifier = TestBaseNotifier()
+        result = notifier.send_push_notification(message="Test message")
+        self.assertEqual(result, "Push notification sent")
---
+++ tests/client_test.py
@@ -0,0 +1,24 @@
+import os
+from unittest import TestCase
+
+from dotenv import load_dotenv
+
+from client import NotifierClient
+from notifiers.aws.notifier import AWSNotifier
+
+
+class TestNotifierClient(TestCase):
+    """TestNotifierClient"""
+
+    def setUp(self):
+        """Set up"""
+        load_dotenv("../.env")
+
+    def test_get_aws_notifier_returns_aws_notifier(self):
+        """Test get_aws_notifier"""
+        aws_notifier = NotifierClient.get_aws_notifier(
+            aws_access_key_id=os.environ.get("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.environ.get("AWS_SECRET_KEY"),
+            region_name="us-east-2",
+        )
+        self.assertIsInstance(aws_notifier, AWSNotifier)
